f99b20748e11246132e248a760be670dcbe3b757
jhonatan-sudo <jhonatancarvajal911@gmail.com>
2025-09-03 13:05:12 -0500

    Menu_opciones

---
+++ Menu.py
@@ -0,0 +1,16 @@
+import math
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+while True:    
+    
+    print("""
+    Indique la operación a realizar:
+    
+    1. Resta 
+    2. Suma
+    3. Multiplicación
+    4. División
+
+          """)
+    opcion = input("Ingrese la opción deseada (1-4): ")
