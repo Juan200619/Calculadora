0486663e047219623ca1c3cd4a019651f1da15dd
jhonatan-sudo <jhonatancarvajal911@gmail.com>
2025-09-03 13:17:39 -0500

    prueva 2

--- Menu.py
+++ Menu.py
@@ -3,14 +3,16 @@ num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 
 while True:    
-    
-    print("""
-    Indique la operación a realizar:
-    
-    1. Resta 
-    2. Suma
-    3. Multiplicación
-    4. División
+      
+      print("""
+      Indique la operación a realizar:
+      
+      1. Resta 
+      2. Suma
+      3. Multiplicación
+      4. División
+      5. raiz cuadrada
+      6. potencia
 
-          """)
-    opcion = input("Ingrese la opción deseada (1-4): ")
+            """)
+      opcion = input("Ingrese la opción deseada (1-6): ")
