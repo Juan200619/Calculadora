3be1f60cab1e8c2b0b166c807a4b91fa1bd7b662
jhonatan-sudo <jhonatancarvajal911@gmail.com>
2025-09-03 13:33:58 -0500

    la multiplicacion y division

--- Menu.py
+++ Menu.py
@@ -16,3 +16,13 @@ while True:
 
             """)
       opcion = input("Ingrese la opción deseada (1-6): ")
+      
+      if opcion == "3":
+            resultado = num1 * num2
+            print(f"El resultado de la multiplicación es: {resultado}")
+      if opcion == "4":
+            if num2 != 0:
+                  resultado = num1 / num2
+                  print(f"El resultado de la división es: {resultado}")
+            else:
+                  print("Error: División por cero no permitida.")
